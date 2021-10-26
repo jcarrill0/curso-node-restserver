c76e4c7a7946a4681e1c617551e02c0d291b81be
jcarrill0 <josecarrillo8@gmail.com>
2021-10-26 12:54:56 -0600

    Upate config DB

--- database/config.js
+++ database/config.js
@@ -1,22 +1,17 @@
 const mongoose = require('mongoose')
 
 
-
 const dbConnection = async () => {
     try {
-
         await mongoose.connect(process.env.MONGODB_CNN, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
-        
         console.log('Base de datos online')
-
     } catch (error) {
         console.log(error)
         throw new Error('Error al inicializar la base de datos')
     }
-
 }
 
 module.exports = {
