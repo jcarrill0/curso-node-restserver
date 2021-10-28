52b775548392ab40ec80a9fcc48b3b3cfd0f18ba
jcarrill0 <josecarrillo8@gmail.com>
2021-10-27 23:58:10 -0600

    Update app usuarios

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,9 +2,14 @@ const { Router } = require('express')
 const { check } = require('express-validator')
 
 const { correoExiste, esRoleValidado, existeUsuarioPorId } = require('../helpers/db-validator')
-const { validarCampos } = require('../middlewares/validar-campos')
-const { validarJWT } = require('../middlewares/validar-jwt')
-const { esAdminRole, tieneRole } = require('../middlewares/validar-roles')
+
+const { 
+    validarCampos,
+    validarJWT,
+    esAdminRole,
+    tieneRole
+ } = require('../middlewares');
+
 const { 
     usuariosGet, 
     usuariosPut, 
